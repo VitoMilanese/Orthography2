39cf3f3873c0c41af93c630e7d52faacb3019824
Vitalii Hanych <vitalii.hanych@gmail.com>
2020-06-01 20:05:29 +0200

    Orthography - blazor server app (supporting .net core)

---
+++ Orthography/Models/Word.cs
@@ -0,0 +1,10 @@
+﻿namespace Orthography.Models
+{
+	public class Word
+	{
+		public int ID { get; set; }
+		public string Value { get; set; }
+		public int PrepositionsMask { get; set; }
+		public string Translation { get; set; }
+	}
+}
