8b51182b837af58d9bbdf10a55eec3e951047a27
Vitalii Hanych <vitalii.hanych@gmail.com>
2020-06-01 16:35:53 +0200

    Project uploaded

---
+++ Orthography2/Enums.cs
@@ -0,0 +1,35 @@
+﻿namespace Orthography.Enums
+{
+	public enum Person
+	{
+		Any = 0,
+		First = 1,
+		Second = 2,
+		Third = 3
+	}
+
+	public enum Number
+	{
+		Any = 0,
+		Singular = 1,
+		Plural = 2
+	}
+
+	public enum Gender
+	{
+		Any = 0,
+		Male = 1,
+		Female = 2
+	}
+
+	public enum Mode
+	{
+		Random = 0,
+		Present = 2,
+		Imperfect = 8,
+		Future = 14,
+		Imperative = 20,
+		Participle = 23,
+		Translation = 27
+	}
+}
---
+++ Orthography2/Models/IdValue.cs
@@ -0,0 +1,18 @@
+﻿namespace Orthography.Models
+{
+	public class IdValue
+	{
+		public int ID { get; set; }
+		public string Value { get; set; }
+
+		public IdValue()
+		{
+		}
+
+		public IdValue(int id, string value)
+		{
+			ID = id;
+			Value = value;
+		}
+	}
+}
