3c5cf18fa58903a542466925bc5e2ff6a11be4be
Vitalii Hanych <vitalii.hanych@gmail.com>
2020-06-02 14:58:19 +0200

    Code cleanup

--- Orthography/Shared/Helper.cs
+++ Orthography/Shared/Helper.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Orthography.Shared
+﻿namespace Orthography.Shared
 {
 	public static class Helper
 	{
