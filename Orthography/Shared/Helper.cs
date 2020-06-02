0876d0971eb984c138793e91ec8bdbe533a59680
Vitalii Hanych <vitalii.hanych@gmail.com>
2020-06-02 14:41:21 +0200

    Helper class

---
+++ Orthography/Shared/Helper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Orthography.Shared
+{
+	public static class Helper
+	{
+        public static int ArrayToBitmask(bool[] array)
+        {
+            var bitmask = 0;
+            for (var i = array.Length - 1; i >= 0; --i)
+            {
+                bitmask |= array[i] ? 1 : 0;
+                if (i > 0) bitmask <<= 1;
+            }
+            return bitmask;
+        }
+
+        public static bool[] BitmaskToArray(int bitmask, int arraySize)
+        {
+            var res = new bool[arraySize];
+            for (var i = 0; i < arraySize; ++i)
+            {
+                res[i] = bitmask % 2 > 0;
+                bitmask >>= 1;
+            }
+            return res;
+        }
+    }
+}
