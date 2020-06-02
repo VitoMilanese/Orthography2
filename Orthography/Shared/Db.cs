ae9f6212508bd4b9b16d941bcacea1fdb1e1bad9
Vitalii Hanych <vitalii.hanych@gmail.com>
2020-06-02 14:59:10 +0200

    Static DB context

---
+++ Orthography/Shared/Db.cs
@@ -0,0 +1,19 @@
+﻿using DataLayer;
+
+namespace Orthography.Shared
+{
+	public class Db : DataContext
+	{
+		private const string ConnectionString = @"Data Source=192.168.1.3\SQLSERVER;Initial Catalog=Orthography;Persist Security Info=True;User ID=sa;Password=developer";
+		public static Db Context;
+
+		static Db()
+		{
+			Context = new Db();
+		}
+
+		private Db() : base(ConnectionString)
+		{
+		}
+	}
+}
