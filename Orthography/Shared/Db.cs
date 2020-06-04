e39f9518f6a9715ea6943617724bdbdc5192611a
Vitalii Hanych <vitalii.hanych@gmail.com>
2020-06-04 15:48:12 +0200

    launchSettings and ConnectionString updated

--- Orthography/Shared/Db.cs
+++ Orthography/Shared/Db.cs
@@ -5,6 +5,7 @@ namespace Orthography.Shared
 	public class Db : DataContext
 	{
 		private const string ConnectionString = @"Data Source=192.168.1.3\SQLSERVER;Initial Catalog=Orthography;Persist Security Info=True;User ID=sa;Password=developer";
+		//private const string ConnectionString = @"Data Source=vhanychc.w12.hoster.by;Initial Catalog=vhanychc_Orthography;Persist Security Info=True;User ID=vhanychc_developer;Password=D3v310p3r";
 		public static Db Context;
 
 		static Db()
