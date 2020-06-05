750dd37fa7952399cdc8e5181579cd23376b4f42
Vitalii Hanych <vitalii.hanych@gmail.com>
2020-06-05 19:02:22 +0200

    Sensitive data hidden

--- Orthography/Shared/Db.cs
+++ Orthography/Shared/Db.cs
@@ -4,12 +4,7 @@ namespace Orthography.Shared
 {
 	public class Db : DataContext
 	{
-#if RELEASE
-		private const string ConnectionString = @"Data Source=vhanychc.w12.hoster.by;Initial Catalog=vhanychc_Orthography;Persist Security Info=True;User ID=vhanychc_developer;Password=D3v310p3r";
-#else
-		//private const string ConnectionString = @"Data Source=localhost\SQLSERVER;Initial Catalog=Orthography;Persist Security Info=True;User ID=sa;Password=developer";
 		private const string ConnectionString = @"Data Source=192.168.1.3\SQLSERVER;Initial Catalog=Orthography;Persist Security Info=True;User ID=sa;Password=developer";
-#endif
 		public static Db Context;
 
 		static Db()
