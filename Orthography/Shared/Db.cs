9eabeb5e9fe9924320da80c933f5a7c2dc2e181d
Vitalii Hanych <vitalii.hanych@gmail.com>
2020-06-12 00:35:03 +0200

    Local IP changed

--- Orthography/Shared/Db.cs
+++ Orthography/Shared/Db.cs
@@ -4,7 +4,7 @@ namespace Orthography.Shared
 {
 	public class Db : DataContext
 	{
-		private const string ConnectionString = @"Data Source=192.168.1.3\SQLSERVER;Initial Catalog=Orthography;Persist Security Info=True;User ID=sa;Password=developer";
+		private const string ConnectionString = @"Data Source=192.168.1.111\SQLSERVER;Initial Catalog=Orthography;Persist Security Info=True;User ID=sa;Password=developer";
 		public static Db Context;
 
 		static Db()
