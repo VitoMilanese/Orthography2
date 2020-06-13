c5a1020fb85ee01da1a3290d3bdc83ba01fa0fef
Vitalii Hanych <vitalii.hanych@gmail.com>
2020-06-13 15:30:30 +0200

    Updated the WebAPI link for Mobile App

--- Mobile/OrthographyMobile/App.xaml.cs
+++ Mobile/OrthographyMobile/App.xaml.cs
@@ -10,7 +10,11 @@ namespace OrthographyMobile
 		//To debug on Android emulators run the web backend against .NET Core not IIS
 		//If using other emulators besides stock Google images you may need to adjust the IP address
 		public static string AzureBackendUrl =
-			DeviceInfo.Platform == DevicePlatform.Android ? "http://10.0.2.2:5000" : /*"http://192.168.1.111:51975"; //*/"http://vhanych.com";
+			DeviceInfo.Platform == DevicePlatform.Android ? "http://10.0.2.2:5000"
+			//: "http://192.168.1.111:51975";
+			//: "http://192.168.1.111:81";
+			//: "http://vhanych.com";
+			: "http://vitomilanese-001-site1.gtempurl.com";
 		public static bool UseMockDataStore = false;
 
 		public App()
