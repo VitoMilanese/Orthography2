cb78ac5b46067f8f549c2ed37945e88eee90f359
Vitalii Hanych <vitalii.hanych@gmail.com>
2020-06-14 22:45:43 +0200

    Prepositions view: checkboxes enhanced

---
+++ Orthography/Shared/CheckBox.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Orthography.Shared
+{
+	public class CheckBox
+    {
+        private bool m_checked;
+        public bool Checked
+        {
+            get => m_checked;
+            set
+            {
+                if (!Enabled) return;
+                m_checked = value;
+                OnChanged?.Invoke(this, value);
+            }
+        }
+
+        public bool Enabled { get; set; } = true;
+
+        public EventHandler<bool> OnChanged { get; set; }
+    }
+}
